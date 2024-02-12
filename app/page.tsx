d532931f9b1fe92894b0f88cb270dca352edbe9e
rak <koenigryan4@gmail.com>
2024-02-12 18:24:23 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
+import Head from 'next/head'
+
+
 export default function Home() {
+
+
+
   return (
+    
+
     <main className="homepage">
       <div>
+        <Head>
+          <title> My website yippee</title>
+        </Head>
         <p> Hello World</p>
       </div>
       
