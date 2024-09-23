1143586d2fd15dd6cebd2f72a6b02c182b5f2d6b
rak <koenigryan4@gmail.com>
2024-09-23 15:37:37 +0100

    Foundation for home page

--- portsite/src/Navbar.jsx
+++ portsite/src/Navbar.jsx
@@ -1,24 +1,53 @@
 import React from "react"; 
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import iconimg from './icon1.png';
 
 function Navbar() {   
     return (   
-        <div className="stick"> 
+        <div style = {stick}> 
             <nav>
-                {icon()}
-                <Link to="/" className="textlink">home</Link>                   
-                <Link to="/Projects" className="textlink">projects</Link> 
-                <Link to="/About" className="textlink">about</Link>                    
+                {Icon()}
+                <Link to="/" style = {textLink}>home</Link>                   
+                <Link to="/Projects" style = {textLink}>projects</Link> 
+                <Link to="/About" style = {textLink}>about</Link>                    
             </nav>   
         </div> 
     ); 
 } 
 
-function icon() {
-    return      <Link to="/">
-                    <img src={iconimg} alt="RKIcon" draggable="false"/>
-                </Link>
+function Icon() {
+    return  <Link to="/">
+                <img 
+                    src={iconimg} 
+                    alt="Ryan Koenig" 
+                    draggable="false" 
+                    style = {icon}
+                />
+            </Link>
 }
+
+const stick = {
+    display: "flex",
+    backgroundColor: "#141414",
+    position: "fixed",
+    color: "white",
+    left: "0px",
+    right: "0px",
+    top: "0px",
+    textIndent: ".12in",
+    fontSize: "1.5em",
+    zIndex: "5",
+  }
+  
+const textLink = {
+    padding: ".25in",
+    position: "relative",
+    bottom: ".15in",
+  }
+
+const icon = {
+    height:".55in",
+    alignSelf: "bottom",
+  }
 
 export default Navbar; 
--- portsite/src/pages/Home.jsx
+++ portsite/src/pages/Home.jsx
@@ -1,32 +1,156 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import largeIcon from '../icon2.png';
 
 function Home() {     
     return (     
-        <div>  
-            <div className="intro">
-                <img source="largeIcon" draggable="false" alt="Ryan Koenig Icon">
-                
-                </img>
-                <p className="title">
-                    Ryan Koenig 
-                </p>
-                <p className="subtitle">
-                    UCD, BSc Computer Science 
-                    Software Engineer
-                </p>
-            </div>
-            <div className="projectShowcase">
-                <p></p>
-            </div>
-            <div className="about">
-                <p></p>
-            </div>
-            <div className="contact">
-                <p></p>
-            </div>
+        <div style={base}>  
+            {Intro()}
+            <line/>
+            {ProjectShowcase()}
+            <hr />
+            {About()}
+            <hr />
+            {Contact()}
         </div>    
     ); 
 } 
+
+function Intro() {
+    return <div style ={intro}>
+                {Icon()}
+                <div>
+                    <p style = {title}>
+                        Ryan Koenig 
+                    </p>
+                    <p style = {subtitle}>
+                        UCD, BSc Computer Science 
+                        Software Engineer
+                    </p>
+                </div>
+            </div>
+}
+
+function Icon() {
+    return <img 
+            src={largeIcon} 
+            alt="Ryan Koenig" 
+            draggable="false" 
+            style = {me}
+            />
+}
+
+function ProjectShowcase() {
+    return  <div style = {projectShowcase}>
+                    <div style = {video}> peepee </div>
+                    <p> words words words, business business business </p>
+            </div>
+}
+
+function About() {
+    return  <div style = {about}>
+                <p> words words words, business business business </p>
+                <div style = {video}> peepee </div>
+            </div>
+}
+
+function Contact() {
+    return  <div style = {contact}>
+                <a href="mailto:koenigryan4@gmail.com" target="_blank">Email me</a>
+                <a href="tel:+3530877101312" target="_blank">Call me</a>
+                <a href="https://www.linkedin.com/in/ryanakoenig/" target="_blank"> My LinkedIn </a>
+                <a href="https://github.com/therakisback" target="_blank"> My Github </a>
+            </div>
+}
+
+const line = ({ color }) = (
+    <hr style= {{color: color, border: "0px", height: "3px"}} />
+
+)
+
+const base = {
+    display: "grid",
+}
+
+
+const intro = {
+    position: "relative",
+    display: "grid",
+    gridTemplateColumns: "repeat(2, 1fr)",
+    top: "1in",
+    left: "15%",
+    zIndex: "0",
+    backgroundColor: "#303030",
+    paddingLeft: "6%",
+    paddingTop: ".5in",
+    paddingBottom:".5in",
+    paddingRight: "4%",
+    width: "60%",
+    height: "3in",
+    justifyContent: "center"
+};
+
+const title = {
+    position: "relative",
+    top: "0in",
+    fontSize: "3em",
+  }
+
+const subtitle = {
+
+}
+
+const me = {
+    height: "3in",
+    display: "inline",
+  }
+
+const projectShowcase = {
+    backgroundColor: "#303030",
+    position: "relative",
+    display: "grid",
+    gridTemplateColumns: "40% auto",
+    gridGap: "10%",
+    top: "2in",
+    left: "7%",
+    zIndex: "0",
+    width: "86%",
+    paddingTop: ".5in",
+    paddingBottom: ".5in",
+}
+
+const video = {
+    backgroundColor: "#ffffff",
+}
+
+const about = {
+    backgroundColor: "#303030",
+    position: "relative",
+    display: "grid",
+    gridTemplateColumns: "auto 40%",
+    gridGap: "10%",
+    top: "2in",
+    left: "7%",
+    zIndex: "0",
+    width: "86%",
+    paddingTop: ".5in",
+    paddingBottom: ".5in",
+}
+
+const contact = {
+    backgroundColor: "#303030",
+    position: "relative",
+    display: "grid",
+    gridTemplateColumns: "10% 10% 10% 10%",
+    justifyItems: "center",
+    alignItems: "bottom",
+    justifyContent: "center",
+    gridGap: "10%",
+    top: "2in",
+    left: "7%",
+    zIndex: "0",
+    width: "86%",
+    paddingTop: ".5in",
+}
 
 export default Home;
